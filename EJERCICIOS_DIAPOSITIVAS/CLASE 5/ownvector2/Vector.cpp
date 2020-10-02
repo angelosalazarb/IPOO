bd46dd86e9bcec854daf598b40ebb2bcd6c7a99e
angelosalazarb <angelosalazarb@gmail.com>
2020-10-01 20:53:02 -0500

    Created Cpp

--- EJERCICIOS_DIAPOSITIVAS/CLASE 5/ownvector2/Vector.cpp
+++ EJERCICIOS_DIAPOSITIVAS/CLASE 5/ownvector2/Vector.cpp
@@ -26,4 +26,79 @@ Vector::Vector(int nSize, int fill){
 
 Vector::~Vector(){}
 
+void Vector::setSize(int nSize){
+  size =  nSize;
+}
+
+int Vector::getSize(){
+  return size;
+}
+
+int Vector::getValue(int index){
+  return buffer[index];
+}
+
+void Vector::consoleFill(){
+  int value;
+  
+  for (int index = 0; index < getSize(); index++){
+    cin >> value;
+    buffer[index] = value;
+    pivot++;
+  }
+}
+
+void Vector::set(int index,int value){
+  buffer[index] = value;
+  pivot++;
+}
+
+void Vector::printVector(){
+  cout << "[";
+  for (int index = 0; index < getSize(); index++){
+    if(buffer[index+1] < getSize()){
+      cout << buffer[index] << ",";
+    }
+    else{
+      cout << buffer[index];
+    }
+  }
+  cout << "]";
+}
+
+void Vector::order(){
+  int aux;
+  for (int index = 0; index < getSize(); index++){
+    for(int i = 1; index < getSize(); i++){
+      if(buffer[i+1] > buffer[i]){
+        aux = buffer[i];
+        set(i+1, buffer[i+1]);
+        set(i, buffer[aux]);
+      }
+    }
+  }
+}
+
+void Vector::sort(){
+  string op;
+  cin >> op;
+
+  int counter = 0;
+  if(op == "asc"){
+    for(int index = 0; index < getSize(); index++){
+      buffer[index] = buffer[counter];
+      counter++;
+    }
+  }
+
+  else if(op == "desc"){
+    for(int index = getSize()-1; index == 0 ; index--){
+      buffer[index] = buffer[counter];
+      counter++;
+    }
+  }
+}
+
+
+
 
