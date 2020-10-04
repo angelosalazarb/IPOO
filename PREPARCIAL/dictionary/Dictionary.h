c537a4ab6cab41d11c2e9a2917412775b85239c6
angelosalazarb <angelosalazarb@gmail.com>
2020-10-03 22:49:51 -0500

    Dictionary.h

---
+++ PREPARCIAL/dictionary/Dictionary.h
@@ -0,0 +1,75 @@
+/**
+ * @file Dictionary.h
+ * @author Ángelo Salazar (angelo.salazar@correounivalle.edu.co)
+ * @version 0.1
+ * @date 2020-10-03
+ */
+#ifndef DICTIONARY_H_
+#define DICTIONARY_H_
+#include <string>
+using namespace std;
+
+/**Define the class dictionary that will save objects it have fields that store info, workin like a database*/
+class Dictionary
+{
+private:
+  
+  string field;
+  string value;
+
+  string components[10000];
+  string buffer1[10000];
+  string buffer2[10000];
+
+public:
+  /**
+   * Construct a new Dictionary object
+   * @contract <Dictionary><> ---> <obj>
+   * @see Dictionary(string nField, string nValue)
+   */
+  Dictionary();
+
+  /**
+   * Construct a new Dictionary object
+   * @contract <Dictionary><str><str> ---> <obj>
+   * 
+   * @param nField is the new field
+   * @param nValue is the new value assigned to a field
+   */
+  Dictionary(string nField, string nValue);
+
+  /**
+   * Destroy the Dictionary object
+   * @contract <><> ---> <>
+   * 
+   */
+  ~Dictionary();
+
+  /**--** getters and setter **--**/
+
+  /**
+   * Get the Field of the object
+   * @contract <><> ---> <>
+   * 
+   * @return string 
+   */
+  string getField();
+
+  void setField(string nField);
+
+  string getValue(string field);
+
+  void setValue(string nValue);
+
+  /**--** others methods **--**/
+
+  void fields();
+
+  void lookUp();
+
+  void setValueInField(string field, string nValue);
+
+
+};
+
+#endif
